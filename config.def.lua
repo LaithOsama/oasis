1c7c87dba694279a05508c2c057a8d9c2db30527
laith <laith@disroot.org>
2024-05-15 02:32:13 +0300

    modify: config.def.lua

--- config.def.lua
+++ config.def.lua
@@ -15,7 +15,8 @@ return {
                 -- Each entry contains a list of packages, a list of patterns to
                 -- include, and a list of patterns to exclude. If no patterns
                 -- are specified, all files from the package are included.
-		{sets.core, exclude={'^include/', '^lib/.*%.a$'}},
+		{sets.core, sets.extra, sets.media, sets.net, sets.desktop, exclude={'^include/', '^lib/.*%.a$'}},
+		{sets.devel},
 
 		-- You may also specify a list of patterns to include or exclude
 		-- for any packages not matching any entries. If no patterns are
@@ -38,12 +39,12 @@ return {
 
 	-- output git repository
 	repo={
-		path='$builddir/root.git',
-		flags='--bare',
+		path='../..',
+		flags='',
 		tag='tree',
-		branch='master',
+		branch='oasis',
 	},
 
 	-- GPU driver (possible_values: amdgpu intel nouveau)
-	-- video_drivers={intel=true}
+	video_drivers={intel=true}
 }
